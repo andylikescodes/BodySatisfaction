95c7ce70ac563a26bdc2ba8968c23c5d4aa66dbc
Dehua Liang <andylikescodes@gmail.com>
2019-06-19 02:59:43 -0700

    added bash script

---
+++ scripts/woman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python woman.py
+
+aws s3 cp ../outputs/woman_aggregate.csv s3://body-satisfaction/woman_aggregate.csv
+
+aws s3 cp ../outputs/woman_raw.csv s3://body-satisfaction/woman_raw.csv
+
+sudo shutdown -P now
